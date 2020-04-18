e574cd06b81c75a97d7f4fc5b782a35ddc36426f
arpangroup <arpangroup1@gmail.com>
2020-04-18 12:27:56 +0530

    Initial Commit

---
+++ src/test/java/com/arpangroup/jwt/api/SpringSecuriryJwtExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpangroup.jwt.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuriryJwtExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
